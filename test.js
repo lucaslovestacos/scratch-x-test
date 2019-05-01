ea11dba6f62a25e4e876b07363f69c47367f6711
lucaslovestacos <35196828+lucaslovestacos@users.noreply.github.com>
2019-05-01 09:45:19 -0400

    Update test.js

--- test.js
+++ test.js
@@ -8,17 +8,18 @@
         return {status: 2, msg: 'Ready'};
     };
 
-        ext.my_first_block = function() {
+    ext.my_first_block = function() {
         // Code that gets executed when the block is run
     };
-    
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['', 'My First Block', 'my_first_block'],
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('First Extention', descriptor, ext);
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
