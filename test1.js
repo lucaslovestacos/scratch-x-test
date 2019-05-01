cedf6a9621892704087ac15360248f30565ac47c
lucaslovestacos <35196828+lucaslovestacos@users.noreply.github.com>
2019-05-01 09:35:07 -0400

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,13 @@
+// Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['r', '%n ^ %n', 'power', 2, 3],
+            ['r', 'infinity', 'inf', ''],
+            ['r', 'negative infinity', 'neg_inf', ''],
+            ['b', '%n is finite?', 'inf_b'],
+            ['r', 'remainder of %n divided by %n', 'mod', 10],
+            
+        ],
+        url:'http://gw90.github.io/My-Scratch-Extension/'
+    };
---
+++ test3.js
@@ -0,0 +1,43 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+    
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+    
+    ext.inf = function() {
+        return Infinity;
+    };
+    
+    ext.neg_inf = function() {
+        return -Infinity;
+    };
+    
+    ext.inf_b = function(num) {
+        return isFinite(num);
+    };
+    
+    ext.mod = function(uno, dos) {
+        return uno % dos;
+    };
+    
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['r', '%n ^ %n', 'power', 2, 3],
+            ['r', 'infinity', 'inf', ''],
+            ['r', 'negative infinity', 'neg_inf', ''],
+            ['b', '%n is finite?', 'inf_b'],
+            ['r', 'remainder of %n divided by %n', 'mod', 10],
+            
+        ],
+        url:'http://gw90.github.io/My-Scratch-Extension/'
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Math', descriptor, ext);
+    
+    
+})({});
